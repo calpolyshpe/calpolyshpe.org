eac12e3691abfca13022e1fe6d48e1721b27574f
Julissa <jhern430@calpoly.edu>
2024-09-21 13:48:51 -0700

    Made the footer functional, was fixing the header a bit more

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -1,9 +1,9 @@
 import Link from 'next/link'
-import { FaInstagram } from "react-icons/fa";
-import { FaDiscord } from "react-icons/fa";
-import { FaLinkedin } from "react-icons/fa";
+import { useRouter } from 'next/router';
+import { FaInstagram, FaDiscord, FaLinkedin } from "react-icons/fa";
 
 export default function Footer() {
+
 
     return (
         <div className="bg-[#001F5B] flex flex-row justify-between items-center py-2 px-3 border-t border-white border-opacity-50 relative">
@@ -12,14 +12,19 @@ export default function Footer() {
                 className="w-1/5 h-auto"
                 alt="SHPE Cal Poly Logo"
             />
-            <Link href="/officers" className="text-white hover:text-[#8f8f8f] lg:text-[14px] text-[11px]"> Become a Member</Link>
-            <Link href="/officers" className="text-white hover:text-[#8f8f8f] lg:text-[14px] text-[11px]"> Officers</Link>
+            <Link href="/Members/BecomeAMember" className={`text-white hover:text-[#8f8f8f] lg:text-[14px] text-[11px]`}> Become a Member</Link>
+            <Link href="/officers" className="text-white hover:text-[#8f8f8f] lg:text-[14px] text-[11px] onPage"> Officers</Link>
             <Link href="/sponsor" className="text-white hover:text-[#8f8f8f] lg:text-[14px] text-[11px]"> Partners</Link>
             <Link href="/about" className="text-white hover:text-[#8f8f8f] lg:text-[14px] text-[11px]"> About Us</Link>
-            <FaDiscord className='text-white w-7 h-auto'/>
-            <FaInstagram className='text-white w-7 h-auto'/>
-            <FaLinkedin className='text-white w-7 h-auto'/>
-
+            <a href='https://discord.gg/vTtAc29KkR'>
+                <FaDiscord className='text-white w-7 h-auto' />
+            </a>
+            <a href='https://www.instagram.com/shpecalpoly/?hl=en'>
+                <FaInstagram className='text-white w-7 h-auto' />
+            </a>
+            <a href='https://www.linkedin.com/company/shpe-cal-poly/posts/?feedView=all'>
+                <FaLinkedin className='text-white w-7 h-auto' />
+            </a>
         </div>
     )
 }
--- src/components/Header.js
+++ src/components/Header.js
@@ -11,14 +11,22 @@ export default function Header() {
                     alt="SHPE Cal Poly Logo"
                 />
             </Link>
-            <div className="absolute justify-between hidden md:flex right-0 transform -translate-y-1/2 top-1/2" >
+            <div className="absolute justify-between hidden md:flex w-[550px] right-0 transform -translate-y-1/2 top-1/2" >
                 <button className="text-white hover:text-[#8f8f8f] lg:text-[14px] text-[11px] md:mr-2 mr-1"> Members</button>
-                <Link href="/officers" className="text-white hover:text-[#8f8f8f] lg:text-[14px] text-[11px] md:mr-2 mr-1"> Officers</Link>
-                <Link href="/sponsor" className="text-white hover:text-[#8f8f8f] lg:text-[14px] text-[11px] md:mr-2 mr-1"> Partners</Link>
-                <Link href="/about" className="text-white hover:text-[#8f8f8f] lg:text-[14px] text-[11px] md:mr-2 mr-1"> About Us</Link>
-                <button className="text-white bg-[#FD652F] rounded lg:text-[14px] text-[11px] md:mr-2 mr-1 px-1 md:py-0.5 py-0.25">
-                    Become a Member
-                </button>
+                <Link href="/officers">
+                    <button className="text-white hover:text-[#8f8f8f] lg:text-[14px] text-[11px] md:mr-2 mr-1">Officers</button>
+                </Link>
+                <Link href="/sponsor">
+                    <button className="text-white hover:text-[#8f8f8f] lg:text-[14px] text-[11px] md:mr-2 mr-1">Partners</button>
+                </Link>
+                <Link href="/about">
+                    <button className="text-white hover:text-[#8f8f8f] lg:text-[14px] text-[11px] md:mr-2 mr-1">About Us</button>
+                </Link>
+                <Link href="/Members/BecomeAMember">
+                    <button className="text-white bg-[#FD652F] rounded lg:text-[14px] text-[11px] md:mr-3 mr-1 px-1 md:py-0.5 py-0.25">
+                        Become a Member
+                    </button>
+                </Link>   
             </div>
             <div className="md:hidden absolute right-0 w-5 h-5 mr-2 transform -translate-y-1/2 top-1/2">
                 <svg className="text-white size-6" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth="1.5" stroke="currentColor" >
