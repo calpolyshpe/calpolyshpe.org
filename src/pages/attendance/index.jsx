f1fbb5b70e0189721e9a3a959ffbdc24c4c1e9e3
Asarel Castellanos <asarelc@gmail.com>
2024-09-22 10:04:28 -0700

    attendance form

---
+++ src/pages/attendance/index.jsx
@@ -0,0 +1,11 @@
+export default function Attendance() {
+  return (
+    <div>
+        <h1>Cal Poly SHPE Attendance</h1>
+      <div>
+        <button>Create New Member</button>
+        <button>Take Attendance</button>
+      </div>
+    </div>
+  );
+}
