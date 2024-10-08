cf258b61181c9ce2c03f7f56ecda393cd1921e44
Asarel Castellanos <asarelc@gmail.com>
2024-10-08 16:58:14 -0700

    redirect /donate to our crowdfunding page

--- next.config.mjs
+++ next.config.mjs
@@ -1,6 +1,15 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
+    async redirects() {
+      return [
+        {
+          source: '/donate',
+          destination: 'https://crowdfund.calpoly.edu/project/44011',
+          permanent: true, // Set to true if this is a permanent redirect (301), or false for a temporary redirect (302)
+        },
+      ]
+    },
 };
 
 export default nextConfig;
